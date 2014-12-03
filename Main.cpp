71b5c77b1e8989b6335b2706057e2ff5524b78d8
Steve Costa <stevencosta@gmail.com>
2014-12-02 21:12:02 -0500

    Initial checkin for sdl template

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char **argv){
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Quit();
+
+	return 0;
+}
